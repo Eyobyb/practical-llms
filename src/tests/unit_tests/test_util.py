5ee37f8d29f2cbfef319289ece7f8b9183b5f4a9
Eyobyb <eyobyirgu@gmail.com>
2023-10-04 15:39:12 +0300

    changed

--- src/tests/unit_tests/test_util.py
+++ src/tests/unit_tests/test_util.py
@@ -1,44 +1,44 @@
-# import pytest
-# from unittest.mock import patch,Mock
+import pytest
+from unittest.mock import patch,Mock
 
-# from sherpa_ai.utils import get_base_url, get_links_from_string, log_formatter, question_reconstructor, scarape_with_url, show_commands_only
+from sherpa_ai.utils import get_base_url, get_links_from_string, log_formatter, question_reconstructor, scarape_with_url, show_commands_only
 
 
-# def test_get_links_from_string():
-#     text_with_link = "this is the link for ui/ux <https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d> , <http://codepen.io/trending> "
-#     return_data = get_links_from_string(text_with_link)
-#     assert str(return_data) == "[{'url': 'https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d', 'base_url': 'https://ui8.net'}, {'url': 'http://codepen.io/trending', 'base_url': 'http://codepen.io'}]"
+def test_get_links_from_string():
+    text_with_link = "this is the link for ui/ux <https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d> , <http://codepen.io/trending> "
+    return_data = get_links_from_string(text_with_link)
+    assert str(return_data) == "[{'url': 'https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d', 'base_url': 'https://ui8.net'}, {'url': 'http://codepen.io/trending', 'base_url': 'http://codepen.io'}]"
 
-# def test_get_base_url():
-#     data = "https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d"
-#     return_data = get_base_url(data)
-#     assert str(return_data)=="https://ui8.net"
+def test_get_base_url():
+    data = "https://ui8.net/artpaperdsgn/products/e-commerce-shopping-and-marketing-3d"
+    return_data = get_base_url(data)
+    assert str(return_data)=="https://ui8.net"
 
-# def test_successful_request():
-#     # Create a mock for the 'requests.get' function
-#     mock_get = Mock()
-#     mock_get.return_value.status_code = 200
-#     mock_get.return_value.content = b'<html><body>Hello, World!</body></html>'
+def test_successful_request():
+    # Create a mock for the 'requests.get' function
+    mock_get = Mock()
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.content = b'<html><body>Hello, World!</body></html>'
 
-#     # Patch 'requests.get' to return the mock
-#     with patch('requests.get', mock_get):
-#         result = scarape_with_url('http://example.com')
+    # Patch 'requests.get' to return the mock
+    with patch('requests.get', mock_get):
+        result = scarape_with_url('http://example.com')
     
 
-#     assert result['status'] == 200
-#     assert result['data'] == 'Hello, World!'
+    assert result['status'] == 200
+    assert result['data'] == 'Hello, World!'
 
-# def test_failed_request():
-#     # Create a mock for the 'requests.get' function
-#     mock_get = Mock()
-#     mock_get.return_value.status_code = 404
-#     mock_get.return_value.content = b'Not Found'
+def test_failed_request():
+    # Create a mock for the 'requests.get' function
+    mock_get = Mock()
+    mock_get.return_value.status_code = 404
+    mock_get.return_value.content = b'Not Found'
 
-#     with patch('requests.get', mock_get):
-#         result = scarape_with_url('http://example.com')
+    with patch('requests.get', mock_get):
+        result = scarape_with_url('http://example.com')
 
-#     assert result['status'] == 404
-#     assert result['data'] == ''
+    assert result['status'] == 404
+    assert result['data'] == ''
 
 # def test_question_reconstructor():
 #     # Test data
