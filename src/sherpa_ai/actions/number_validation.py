df45fcaa55ac6db6692b6749e5f52bedabdefc12
Eyob <eyobyirgu@gmail.com>
2024-07-15 15:42:57 +0300

    chore: Add entity validation and number validation actions

---
+++ src/sherpa_ai/actions/number_validation.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from typing import Any
+
+from sherpa_ai.actions.base import BaseAction
+from sherpa_ai.tools import SearchArxivTool
+from langchain.base_language import BaseLanguageModel
+from sherpa_ai.output_parsers.validation_result import ValidationResult
+from sherpa_ai.utils import (
+    verify_numbers_against_source,
+)
+
+
+class TextSimilarityMethod(Enum):
+    BASIC = 0
+    METRICS = 1
+    LLM = 2
+
+
+class NumberValidationAction(BaseAction):
+    llm: Any  # The BaseLanguageModel from LangChain is not compatible with Pydantic 2 yet
+
+    # Override the name and args from BaseAction
+    name: str = "Entity Validator"
+    args: dict = {"text": "string", "source": "string"}
+    usage: str = "Validate number in a text by comparing it to the source"
+
+    count: int = 0
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def execute(self, text: str, source: str, **kwargs) -> ValidationResult:
+        """
+        Verifies that all numbers within `text` exist in the `belief` source text.
+
+        Args:
+            text: The text to be analyzed
+            belief: Belief of the Agent that generated `text`
+        Returns:
+            ValidationResult: The result of number validation. If any number in the
+            text to be processed doesn't exist in the source text,
+            validation is invalid and contains a feedback string.
+            Otherwise validation is valid.
+        """
+
+        numbers_exist_in_source, error_message = verify_numbers_against_source(
+            text, source
+        )
+
+        if numbers_exist_in_source:
+            return ValidationResult(
+                is_valid=True,
+                result=text,
+                feedback="",
+            )
+        else:
+            self.count += 1
+            return ValidationResult(
+                is_valid=False,
+                result=text,
+                feedback=error_message,
+            )
+
+    def get_failure_message(self) -> str:
+        return "The numeric value results might not be fully reliable. Exercise caution and consider alternative sources if possible."
